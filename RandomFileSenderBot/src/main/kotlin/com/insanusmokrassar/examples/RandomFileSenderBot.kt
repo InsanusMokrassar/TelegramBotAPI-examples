656b8b67740459fc34f144015ff14e8eae540783
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2020-06-02 19:28:10 +0600

    update

--- RandomFileSenderBot/src/main/kotlin/com/insanusmokrassar/examples/RandomFileSenderBot.kt
+++ RandomFileSenderBot/src/main/kotlin/com/insanusmokrassar/examples/RandomFileSenderBot.kt
@@ -2,10 +2,12 @@ package com.insanusmokrassar.examples
 
 import com.github.insanusmokrassar.TelegramBotAPI.bot.Ktor.KtorRequestsExecutor
 import com.github.insanusmokrassar.TelegramBotAPI.extensions.api.send.sendTextMessage
+import com.github.insanusmokrassar.TelegramBotAPI.types.MessageEntity.textsources.BotCommandTextSource
 import com.github.insanusmokrassar.TelegramBotAPI.types.ParseMode.MarkdownV2
 import com.github.insanusmokrassar.TelegramBotAPI.types.chat.abstracts.ChannelChat
 import com.github.insanusmokrassar.TelegramBotAPI.types.message.*
-import com.github.insanusmokrassar.TelegramBotAPI.types.message.abstracts.PossiblyForwardedMessage
+import com.github.insanusmokrassar.TelegramBotAPI.types.message.abstracts.ContentMessage
+import com.github.insanusmokrassar.TelegramBotAPI.types.message.content.TextContent
 import com.github.insanusmokrassar.TelegramBotAPI.types.update.abstracts.BaseMessageUpdate
 import com.github.insanusmokrassar.TelegramBotAPI.utils.*
 import com.github.insanusmokrassar.TelegramBotAPI.utils.extensions.UpdateReceiver
@@ -25,7 +27,18 @@ suspend fun main(args: Array<String>) {
 
     val callback: UpdateReceiver<BaseMessageUpdate> = { messageUpdate ->
         val message = messageUpdate.data
-        val infoToSend = if (message is PossiblyForwardedMessage) {
+        val infoToSend = if (message is ContentMessage<*>) {
+            val content = message.content
+            when (content) {
+                is TextContent -> {
+                    val commandSource = (content.entities.singleOrNull() ?.source as? BotCommandTextSource)
+                    if (commandSource != null && commandSource.command == "sendFile") {
+                        // TODO
+                    } else {
+                        bot.sendTextMessage(message.chat, "Send me /sendFile to get random file")
+                    }
+                }
+            }
             val forwardInfo = message.forwardInfo
             when (forwardInfo) {
                 null -> "There is no forward info"
@@ -33,7 +46,7 @@ suspend fun main(args: Array<String>) {
                 is UserForwardInfo -> forwardInfo.from.let { user ->
                     "User ${user.id.chatId.toString().codeMarkdownV2()} " + "(${user.firstName} ${user.lastName}: ${user.username ?.username ?: "Without username"})".regularMarkdownV2()
                 }
-                is ForwardFromChannelInfo -> "Channel (".regularMarkdownV2() + (forwardInfo.channelChat).title.codeMarkdownV2() + ")".regularMarkdownV2()
+                is ForwardFromChannelInfo -> "Channel (".regularMarkdownV2() + (forwardInfo.channelChat as ChannelChat).title.codeMarkdownV2() + ")".regularMarkdownV2()
             }
         } else {
             "There is no forward info"
