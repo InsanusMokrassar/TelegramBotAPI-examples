43f50b090a48d757577e269ffc1a29c43e20a2ab
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2020-06-02 20:24:41 +0600

    fixes

---
+++ ForwarderBot/src/main/kotlin/ForwarderBot.kt
@@ -0,0 +1,42 @@
+import com.github.insanusmokrassar.TelegramBotAPI.extensions.api.send.sendTextMessage
+import com.github.insanusmokrassar.TelegramBotAPI.extensions.api.telegramBot
+import com.github.insanusmokrassar.TelegramBotAPI.extensions.utils.formatting.codeMarkdownV2
+import com.github.insanusmokrassar.TelegramBotAPI.extensions.utils.formatting.regularMarkdownV2
+import com.github.insanusmokrassar.TelegramBotAPI.extensions.utils.safely
+import com.github.insanusmokrassar.TelegramBotAPI.extensions.utils.updates.asContentMessagesFlow
+import com.github.insanusmokrassar.TelegramBotAPI.extensions.utils.updates.retrieving.startGettingFlowsUpdatesByLongPolling
+import com.github.insanusmokrassar.TelegramBotAPI.types.ParseMode.MarkdownV2
+import com.github.insanusmokrassar.TelegramBotAPI.types.message.*
+import com.github.insanusmokrassar.TelegramBotAPI.types.message.abstracts.PossiblyForwardedMessage
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
+
+/**
+ * This bot will always return message about forwarder. In cases when sent message was not a forward message it will
+ * send suitable message
+ */
+suspend fun main(vararg args: String) {
+    val botToken = args.first()
+
+    val bot = telegramBot(botToken)
+
+    val scope = CoroutineScope(Dispatchers.Default)
+
+    bot.startGettingFlowsUpdatesByLongPolling(scope = scope) {
+        messageFlow.asContentMessagesFlow().mapNotNull { it as? PossiblyForwardedMessage }.onEach { message ->
+            safely {
+                val toAnswer = when (val forwardInfo = message.forwardInfo) {
+                    null -> "There is no forward info"
+                    is AnonymousForwardInfo -> "Anonymous user which signed as \"${forwardInfo.senderName.codeMarkdownV2()}\""
+                    is UserForwardInfo -> forwardInfo.from.let { user ->
+                        "User ${user.id.chatId.toString().codeMarkdownV2()} " + "(${user.firstName} ${user.lastName}: ${user.username ?.username ?: "Without username"})".regularMarkdownV2()
+                    }
+                    is ForwardFromChannelInfo -> "Channel (".regularMarkdownV2() + (forwardInfo.channelChat).title.codeMarkdownV2() + ")".regularMarkdownV2()
+                }
+                bot.sendTextMessage(message.chat, toAnswer, MarkdownV2)
+            }
+        }.launchIn(scope)
+    }
+
+    scope.coroutineContext[Job]!!.join()
+}
---
+++ RandomFileSenderBot/src/main/kotlin/RandomFileSenderBot.kt
@@ -0,0 +1,55 @@
+import com.github.insanusmokrassar.TelegramBotAPI.extensions.api.bot.setMyCommands
+import com.github.insanusmokrassar.TelegramBotAPI.extensions.api.send.media.sendDocument
+import com.github.insanusmokrassar.TelegramBotAPI.extensions.api.send.sendTextMessage
+import com.github.insanusmokrassar.TelegramBotAPI.extensions.api.telegramBot
+import com.github.insanusmokrassar.TelegramBotAPI.extensions.utils.safely
+import com.github.insanusmokrassar.TelegramBotAPI.extensions.utils.updates.filterExactCommands
+import com.github.insanusmokrassar.TelegramBotAPI.extensions.utils.updates.retrieving.startGettingFlowsUpdatesByLongPolling
+import com.github.insanusmokrassar.TelegramBotAPI.requests.abstracts.toInputFile
+import com.github.insanusmokrassar.TelegramBotAPI.types.BotCommand
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import java.io.File
+
+private const val command = "send_file"
+
+/**
+ * This bot will send files inside of working directory OR from directory in the second argument
+ */
+suspend fun main(args: Array<String>) {
+    val botToken = args.first()
+    val directoryOrFile = args.getOrNull(1) ?.let { File(it) } ?: File("")
+
+    fun pickFile(currentRoot: File = directoryOrFile): File? {
+        if (currentRoot.isFile) {
+            return currentRoot
+        } else {
+            return pickFile(currentRoot.listFiles() ?.random() ?: return null)
+        }
+    }
+
+    val bot = telegramBot(botToken)
+    val scope = CoroutineScope(Dispatchers.Default)
+
+    bot.startGettingFlowsUpdatesByLongPolling(scope = scope) {
+        messageFlow.filterExactCommands(Regex(command)).onEach { message ->
+            safely {
+                pickFile() ?.let {
+                    bot.sendDocument(
+                        message.chat.id,
+                        it.toInputFile()
+                    )
+                } ?: bot.sendTextMessage(message.chat.id, "Nothing selected :(")
+            }
+        }.launchIn(scope)
+    }
+
+    safely {
+        bot.setMyCommands(
+            BotCommand(command, "Send some random file in picker directory")
+        )
+    }
+
+    scope.coroutineContext[Job]!!.join()
+}
