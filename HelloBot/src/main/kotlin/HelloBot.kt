1eb9676c1998834e8f627f2136a355520d79989d
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2020-10-04 17:32:50 +0600

    migration onto 0.29.0

--- HelloBot/src/main/kotlin/HelloBot.kt
+++ HelloBot/src/main/kotlin/HelloBot.kt
@@ -1,14 +1,16 @@
-import com.github.insanusmokrassar.TelegramBotAPI.extensions.api.chat.get.getChat
-import com.github.insanusmokrassar.TelegramBotAPI.extensions.api.send.sendTextMessage
-import com.github.insanusmokrassar.TelegramBotAPI.extensions.api.telegramBot
-import com.github.insanusmokrassar.TelegramBotAPI.extensions.utils.formatting.linkMarkdownV2
-import com.github.insanusmokrassar.TelegramBotAPI.extensions.utils.formatting.textMentionMarkdownV2
-import com.github.insanusmokrassar.TelegramBotAPI.extensions.utils.safely
-import com.github.insanusmokrassar.TelegramBotAPI.extensions.utils.updates.retrieving.startGettingFlowsUpdatesByLongPolling
-import com.github.insanusmokrassar.TelegramBotAPI.types.ParseMode.MarkdownV2
-import com.github.insanusmokrassar.TelegramBotAPI.types.User
-import com.github.insanusmokrassar.TelegramBotAPI.types.chat.abstracts.*
-import com.github.insanusmokrassar.TelegramBotAPI.utils.extensions.escapeMarkdownV2Common
+import dev.inmo.tgbotapi.extensions.api.chat.get.getChat
+import dev.inmo.tgbotapi.extensions.api.chat.get.getChatAdministrators
+import dev.inmo.tgbotapi.extensions.api.send.sendTextMessage
+import dev.inmo.tgbotapi.extensions.api.telegramBot
+import dev.inmo.tgbotapi.extensions.utils.formatting.linkMarkdownV2
+import dev.inmo.tgbotapi.extensions.utils.formatting.textMentionMarkdownV2
+import dev.inmo.tgbotapi.extensions.utils.safely
+import dev.inmo.tgbotapi.extensions.utils.updates.retrieving.startGettingFlowsUpdatesByLongPolling
+import dev.inmo.tgbotapi.types.ParseMode.MarkdownV2
+import dev.inmo.tgbotapi.types.User
+import dev.inmo.tgbotapi.types.chat.abstracts.*
+import dev.inmo.tgbotapi.types.toChatId
+import dev.inmo.tgbotapi.utils.extensions.escapeMarkdownV2Common
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
@@ -22,6 +24,10 @@ suspend fun main(vararg args: String) {
     val bot = telegramBot(botToken)
 
     val scope = CoroutineScope(Dispatchers.Default)
+
+    scope.launch {
+        println(bot.getChatAdministrators((-1001433262056L).toChatId()))
+    }
 
     bot.startGettingFlowsUpdatesByLongPolling(scope = scope) {
         messageFlow.onEach {
