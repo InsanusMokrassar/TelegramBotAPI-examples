33b14f320cd5ea04728c9ae8917d5e1caed99930
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2022-10-30 19:57:11 +0600

    update up to 3.3.1

---
+++ ChatAvatarSetter/README.md
@@ -0,0 +1,9 @@
+# ChatAvatarSetter
+
+This bot will set the chat avatar based on the image sent to bot
+
+## Launch
+
+```bash
+../gradlew run --args="BOT_TOKEN"
+```
---
+++ ChatAvatarSetter/src/main/kotlin/ChatAvatarSetter.kt
@@ -0,0 +1,36 @@
+import dev.inmo.micro_utils.coroutines.runCatchingSafely
+import dev.inmo.tgbotapi.bot.ktor.telegramBot
+import dev.inmo.tgbotapi.extensions.api.chat.modify.setChatPhoto
+import dev.inmo.tgbotapi.extensions.api.files.downloadFile
+import dev.inmo.tgbotapi.extensions.api.send.reply
+import dev.inmo.tgbotapi.extensions.behaviour_builder.buildBehaviourWithLongPolling
+import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.onPhoto
+import dev.inmo.tgbotapi.extensions.utils.*
+import dev.inmo.tgbotapi.requests.abstracts.asMultipartFile
+import kotlinx.coroutines.*
+
+suspend fun main(args: Array<String>) {
+    val bot = telegramBot(args.first())
+
+    bot.buildBehaviourWithLongPolling(scope = CoroutineScope(Dispatchers.IO)) {
+        onPhoto {
+            val bytes = downloadFile(it.content)
+            runCatchingSafely {
+                setChatPhoto(
+                    it.chat.id,
+                    bytes.asMultipartFile("sample.jpg")
+                )
+            }.onSuccess { b ->
+                if (b) {
+                    reply(it, "Done")
+                } else {
+                    reply(it, "Something went wrong")
+                }
+            }.onFailure { e ->
+                e.printStackTrace()
+
+                reply(it, "Something went wrong (see logs)")
+            }
+        }
+    }.join()
+}
