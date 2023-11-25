d842dab5b86cf51cdc5bad60001a9b4bfeb38b9a
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2023-11-25 12:44:05 +0600

    update dependencies

--- GetMeBot/src/main/kotlin/GetMeBot.kt
+++ GetMeBot/src/main/kotlin/GetMeBot.kt
@@ -1,11 +1,20 @@
+import dev.inmo.kslog.common.KSLog
+import dev.inmo.kslog.common.LogLevel
+import dev.inmo.kslog.common.defaultMessageFormatter
+import dev.inmo.kslog.common.filter.filtered
 import dev.inmo.tgbotapi.bot.ktor.telegramBot
 import dev.inmo.tgbotapi.extensions.api.bot.getMe
+import dev.inmo.tgbotapi.utils.DefaultKTgBotAPIKSLog
 
 /**
  * This is one of the most easiest bot - it will just print information about itself
  */
 suspend fun main(vararg args: String) {
     val botToken = args.first()
+
+    KSLog.default = KSLog { level: LogLevel, tag: String?, message: Any, throwable: Throwable? ->
+        println(defaultMessageFormatter(level, tag, message, throwable))
+    }
 
     val bot = telegramBot(botToken)
 
