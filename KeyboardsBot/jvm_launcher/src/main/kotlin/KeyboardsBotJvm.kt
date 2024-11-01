86790ee414a7854cb26d4d7dacab61b4ec249ae0
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2024-11-01 23:21:33 +0600

    add copyText sample button

--- KeyboardsBot/jvm_launcher/src/main/kotlin/KeyboardsBotJvm.kt
+++ KeyboardsBot/jvm_launcher/src/main/kotlin/KeyboardsBotJvm.kt
@@ -1,7 +1,21 @@
+import dev.inmo.kslog.common.KSLog
+import dev.inmo.kslog.common.LogLevel
+import dev.inmo.kslog.common.defaultMessageFormatter
+import dev.inmo.kslog.common.setDefaultKSLog
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 suspend fun main(args: Array<String>) {
+    val isDebug = args.any { it == "debug" }
+
+    if (isDebug) {
+        setDefaultKSLog(
+            KSLog { level: LogLevel, tag: String?, message: Any, throwable: Throwable? ->
+                println(defaultMessageFormatter(level, tag, message, throwable))
+            }
+        )
+    }
+
     withContext(Dispatchers.IO) { // IO for inheriting of it in side of activateKeyboardsBot
         activateKeyboardsBot(args.first()) {
             println(it)
